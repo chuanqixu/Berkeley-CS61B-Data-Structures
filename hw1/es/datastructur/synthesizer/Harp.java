77fb493ff3fe3618221fc47bd7684743e0a5ad22
Chuanqi Xu <466018122@qq.com>
2020-09-06 01:28:03 +0800

    completed hw 1

--- hw1/es/datastructur/synthesizer/Harp.java
+++ hw1/es/datastructur/synthesizer/Harp.java
@@ -1,9 +1,6 @@
 package es.datastructur.synthesizer;
 
-import edu.princeton.cs.algs4.StdRandom;
-
-//Note: This file will not compile until you complete task 1 (BoundedQueue).
-public class GuitarString {
+public class Harp {
     /**
      * Constants. Do not change. In case you're curious, the keyword final
      * means the values cannot be changed at runtime.
@@ -15,7 +12,7 @@ public class GuitarString {
     private BoundedQueue<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
-    public GuitarString(double frequency) {
+    public Harp(double frequency) {
         buffer = new ArrayRingBuffer<>((int) Math.round(SR / frequency));
         for (int i = 0; i < buffer.capacity(); i++) {
             buffer.enqueue(0.0);
@@ -37,12 +34,11 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        buffer.enqueue((buffer.dequeue() + buffer.peek()) * DECAY / 2);
+        buffer.enqueue(-(buffer.dequeue() + buffer.peek()) * DECAY / 2);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
         return buffer.peek();
     }
 }
